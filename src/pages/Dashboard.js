69aca600ff6a94578da7416d4da5c81e1e574eff
Britnie Alexis <57149369+alexisbritnie@users.noreply.github.com>
2023-03-20 14:18:41 -0400

    Add files via upload
    worked on dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,30 +1,72 @@
 import { useState} from "react";
 import { Link} from "react-router-dom";
 import React from 'react';
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
 
 export default function Dashboard(){
     return (
-        <div class = "full-screen-container">
+        <div class = "dash-full-screen-container">
         <div class="dashboard-container">
-            <h1 class="dashboard-title">Dashboard</h1>
-            <form class="side-bar-form" >
-               
-
+            <div class="side-bar-form" >
+                <h1 class="dashboard-title">Dashboard</h1>
                 
-
-                <button 
-                type = "submit" 
-                class="login-button">Login
-                </button>
-
-                <div>
-                    <Link to="/reset">Forgot Password</Link>
+                <div class= "dash-profile-card">
+                    <form>
+                    <h1 class="dash-name">Britnie Alexis</h1>
+                    </form>
                 </div>
-                <div>
-                    Don't have an account? <Link to="/signup">Signup</Link> now!
+
+                <div class="dash-button">
+                    <button type = "submit" >My Profile</button>
                 </div>
-            </form>
+
+                <div class="dash-button">
+                    <button type = "submit" >Search</button>
+                </div>
+
+                <div class="dash-button">
+                    <button type = "submit" >My Notes</button>
+                </div>
+
+                <div class="dash-button">
+                    <button type = "submit" >Saved Materials</button>
+                </div>
+                <div class="dash-button">
+                    <button type = "submit" >Settings</button>
+                </div>
+                
+            </div>
+
+            <div class="card-grid">
+                <div class= "dash-class-cards">
+                    <div class="dash-class-card-header card-image">
+                    <img src="https://source.unsplash.com/D9Zow2REm8U" />
+                    CSIS2101</div>
+                    <div class="dash-class-card-body"> fundementals of computer programming</div>
+                </div>
+                <div class= "dash-class-cards">
+                    <div class="dash-class-card-header card-image">
+                    <img src="https://source.unsplash.com/95YRwf6CNw8" />
+                    CSIS2101</div>
+                    <div class="dash-class-card-body"> fundementals of computer programming</div>
+                </div>
+                <div class= "dash-class-cards">
+                    <div class="dash-class-card-header card-image">
+                    <img src="https://source.unsplash.com/95YRwf6CNw8" />
+                    CSIS2101</div>
+                    <div class="dash-class-card-body"> fundementals of computer programming</div>
+                </div>
+                
+            </div>
+
         </div>
       </div>
 
