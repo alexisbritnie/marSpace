98c61f541c869a2afc7a511cd9ad2a854a65afb8
TrmiDo <111090515+TrmiDo@users.noreply.github.com>
2023-04-15 19:31:15 -0400

    Add files via upload

---
+++ src/Classroom.js
@@ -0,0 +1,309 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import React from 'react';
+import { FaArrowCircleUp, FaFileUpload, FaPlusCircle } from "react-icons/fa";
+import { FaArrowCircleDown } from "react-icons/fa";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, setDoc, collection, addDoc, query, where, getDocs, updateDoc, getDoc, arrayUnion, arrayRemove } from 'firebase/firestore/lite';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { onSnapshot } from 'firebase/firestore'
+import Login from "./Login";
+import NavbarLoggedIn from "../NavbarLoggedIn";
+
+
+export default function Classroom() {
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBi28e8xEpJvwGgSGUqWZXvAe9aLfBi8Ow",
+    authDomain: "marspace-1afb7.firebaseapp.com",
+    projectId: "marspace-1afb7",
+    storageBucket: "marspace-1afb7.appspot.com",
+    messagingSenderId: "936429184235",
+    appId: "1:936429184235:web:f1ddee1d9a9e2f17b44aae",
+    measurementId: "G-5SSF4M5BTB"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+  //Storaging
+  const [credit, setCredit] = useState({ username: "", password: "" });
+  const handleChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setCredit((prev) => {
+      return { ...prev, [name]: value }
+    })
+  }
+
+  const addDocu = (event) => {
+    event.preventDefault();
+    addDoc(collection(db, "Credential"), {
+      username: credit.username,
+      password: credit.password
+    })
+  }
+
+  //uploading
+  const [AllDoc, setAllDoc] = useState([]);
+  const [Post, setPost] = useState({ user: (sessionStorage.getItem("username")), title: "", description: "", link: "", Comment: [], class: (sessionStorage.getItem('className')), rate: 0 }); /* where i will set username and class in background */
+
+  const handleChange3 = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setPost((prev) => {
+      return { ...prev, [name]: value }
+    })
+  }
+  function handleAllDoc(e) {
+    e.preventDefault();
+    const pickedFile = e.target.files;
+    setAllDoc(pickedFile);
+  }
+  function SingleUpload(e) {
+    e.preventDefault();
+    const storage = getStorage();
+
+
+
+    const storageRef = ref(storage, 'AllDoc/' + AllDoc[0].name)
+    const uploadTask = uploadBytesResumable(storageRef, AllDoc[0]);
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        // Observe state change events such as progress, pause, and resume
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+          addDoc(collection(db, "Post"), {
+            username: Post.user,
+            title: Post.title,
+            description: Post.description,
+            link: downloadURL,
+            class: Post.class,
+            rate: Post.rate,
+            Comment: Post.Comment
+
+          })
+        });
+      }
+    );
+    
+    getPost()
+    console.log("1")
+  }
+
+
+  //Put into saved class
+  const addSavedClass = async () => {
+    console.log('userid: ', sessionStorage.getItem("userid"))
+    console.log('className: ', sessionStorage.getItem('className'))
+    const AllClassData = query(collection(db, "Classes"), where("className", "==", sessionStorage.getItem('className')))
+    const querynapshot = await getDocs(AllClassData)
+    var classId
+    querynapshot.forEach((doc) => { classId = doc.id })
+    const classRef = doc(db, "Classes", classId)
+    console.log('classId: ', classId)
+    const classnapshot = await getDoc(classRef)
+    const classdata = classnapshot.data()
+    const userRef = doc(db, "Credential", sessionStorage.getItem("userid"))
+    console.log('class data: ', classdata)
+    await updateDoc(userRef, {
+      SavedClass: arrayUnion(classdata)
+    })
+
+  }
+
+  const [savePost, setSavePost] = useState([])
+
+  const getPost = async () => {
+
+    const queryClass = query(collection(db, "Post"), where("class", '==', sessionStorage.getItem("className")))
+    const classroomSnapshot = await getDocs(queryClass)
+    const searchResults = classroomSnapshot.docs.map((doc) => doc.data());
+    setSavePost(searchResults)
+    console.log("2")
+  }
+
+  useEffect(() => {
+    getPost()
+  }, [])
+  const [comment, setComment] = useState({ User: "", Comment: "" })
+  const handleChangeComment = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setComment((prev) => {
+      return { ...prev, [name]: value }
+    })
+  }
+
+  const addComment = async (value) => {
+    var postId
+    const LookingForPostComment = query(collection(db, "Post"), where("link", "==", value));
+    const querySnapshot1 = await getDocs(LookingForPostComment);
+    querySnapshot1.forEach((doc) => { postId = doc.id })
+    const postRef = doc(db, "Post", postId);
+    const snap1 = await getDoc(postRef)
+    const olda = snap1.data().Comment
+    const objectcomment = { Comment: comment.Comment, User: sessionStorage.getItem("username") }
+    olda.push(objectcomment)
+    await updateDoc(postRef, {
+      Comment: olda
+    })
+    setComment({ Comment: "" })
+    getPost()
+  }
+
+  //upvote
+  const upvote = async (postlink) => {
+    var postId
+    const LookingForPostComment = query(collection(db, "Post"), where("link", "==", postlink));
+    const querySnapshot1 = await getDocs(LookingForPostComment);
+    querySnapshot1.forEach((doc) => { postId = doc.id })
+    const postRef = doc(db, "Post", postId);
+    const snap1 = await getDoc(postRef)
+    const oldscore = snap1.data().rate
+    var score = oldscore + 1
+    await updateDoc(postRef, {
+      rate: score
+    }
+    )
+    getPost()
+
+  }
+  //downvote
+  const downvote = async (postlink) => {
+    var postId
+    const LookingForPostComment = query(collection(db, "Post"), where("link", "==", postlink));
+    const querySnapshot1 = await getDocs(LookingForPostComment);
+    querySnapshot1.forEach((doc) => { postId = doc.id })
+    const postRef = doc(db, "Post", postId);
+    const snap1 = await getDoc(postRef)
+    const oldscore = snap1.data().rate
+    var score = oldscore - 1
+    await updateDoc(postRef, {
+      rate: score
+    })
+    getPost()
+
+  }
+
+
+  return (
+    <>
+      <NavbarLoggedIn />
+      <div class="classroom-full-screen-container">
+
+        <form onSubmit={SingleUpload}>
+          <input type="file" multiple name="AllFile" onChange={handleAllDoc} /> {/*choose file */}
+
+        </form>
+
+        <div class="class-title-bar">
+          <div class="class-title-div">
+            <h1 class="class-title">{sessionStorage.getItem('title')}</h1>
+          </div>
+          <div class="addClassButtonDiv">
+            <FaPlusCircle class='addClass' /> {/*add class to dashboard */}
+            <button class="clear-button-title" onClick={addSavedClass}></button>
+          </div>
+
+        </div>
+
+
+
+        <div class="upload-notes-container">
+          <form onSubmit={SingleUpload}>
+            <input class="note-name-box" type="string" name="title" value={Post.password} onChange={handleChange3} placeholder="Enter your note's title:" />
+            <div></div>
+            <input class="note-description-box" type="string" name="description" value={Post.password} onChange={handleChange3} placeholder="Enter a short description" />
+
+            <div className="upload-doc-container">
+              <FaFileUpload class='uploadFileIcon' />
+
+              <label class="clear-button">
+                <input type="file" onChange={handleAllDoc} className="choose-file" /> {/*choose file */}
+              </label>
+            </div>
+
+            <button class="submit-note-button" type="submit">Submit</button>
+          </form>
+        </div>
+
+        {savePost.map((result, index) => (
+          <>
+            <div class="classroom-container">
+              <p class="note-name">{result.title}</p>
+              <p class="note-description">{result.description}</p>
+              <div class="class-notes-container">
+                <embed src={result.link + "#view=FitH"} width="100%" height="900" />
+
+              </div>
+              <div class="note-ratings">
+                <button onClick={() => upvote(result.link)}>Upvote</button>
+                <FaArrowCircleUp class='arrowUp' />
+                <h3>{result.rate}</h3>
+                <button onClick={() => downvote(result.link)}>Downvote</button>
+                <FaArrowCircleDown class='arrowDown' />
+              </div>
+            </div>
+            <div class="discussion-box-container">
+              <h1> Class Discussion</h1>
+              <div class="discussion-box">
+                <div class="comment-box">
+
+                  {result.Comment.map((result2, index) => (
+                    <div key={index}>
+                      <div class="comment-author">
+                        <h3 class="comment-author-font">{result2.User}</h3>
+                      </div>
+                      <p class="comments-font">{result2.Comment}</p>
+
+                    </div>
+
+
+                  ))}
+
+
+                </div>
+
+
+              </div>
+              <div class="division-line"></div>
+              <div class="typing-comment-box">
+                <input type="text" name="Comment" class="comment-input" value={comment.Comment} onChange={handleChangeComment} placeholder="Enter the comment" />
+
+
+                <button onClick={() => addComment(result.link)}
+                  class="send-button"
+                  type="submit">SEND
+
+                </button>
+              </div>
+            </div>
+          </>
+
+
+        ))}
+
+      </div>
+    </>
+  )
+}
---
+++ src/Dashboard.js
@@ -0,0 +1,132 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import React from 'react';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, doc, setDoc, collection, addDoc, query, where, getDocs, getDoc,updateDoc,arrayRemove} from 'firebase/firestore/lite';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import Search from "../Search";
+import ClassCard from "../ClassCard";
+import { getAuth, signOut } from "firebase/auth"
+import { QuerySnapshot } from "firebase/firestore";
+import NavbarLoggedIn from "../NavbarLoggedIn";
+
+
+
+export default function Dashboard() {
+    const navigate = useNavigate();
+    const navigateToProfile = () => { navigate('/profile') };
+    const navigateToNotes = () => { navigate('/notes') };
+    const navigateToSavedMaterials = () => { navigate('/materials') };
+    const navigateToLogout = () => { navigate('/logout') }
+    const navigateToSearchPage = () => { navigate('/SearchPage') }
+    const firebaseConfig = {
+        apiKey: "AIzaSyBi28e8xEpJvwGgSGUqWZXvAe9aLfBi8Ow",
+        authDomain: "marspace-1afb7.firebaseapp.com",
+        projectId: "marspace-1afb7",
+        storageBucket: "marspace-1afb7.appspot.com",
+        messagingSenderId: "936429184235",
+        appId: "1:936429184235:web:f1ddee1d9a9e2f17b44aae",
+        measurementId: "G-5SSF4M5BTB"
+    };
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+    const docRef = doc(db, "Credential", sessionStorage.getItem('userid'))
+
+    console.log(sessionStorage.getItem("userid"))
+    console.log(sessionStorage.getItem("username"))
+    /*console.log(sessionStorage.getItem('token')) */
+    const [saveCl, setsaveCl] = useState([])
+
+
+    useEffect(() => {
+        rando()
+    }, [])
+
+    const rando = async (event) => {
+        const docSnap = await getDoc(docRef)
+        if (docSnap.exists()) {
+            console.log('Doc data', docSnap.data())
+            const info = docSnap.data()
+            setsaveCl(info.SavedClass)
+        } else {
+            console.log('nada')
+        }
+    }
+
+    const navigateToClassroom = () => { navigate('/classroom') }
+
+
+    const goToClass = (title, className) => {
+        console.log(className)
+        sessionStorage.setItem('title', title)
+        sessionStorage.setItem('className', className)
+
+        console.log('class: ', className)
+        console.log('title: ', title)
+        navigateToClassroom()
+
+    }
+    //Remove saved post
+const removeSavedPost= async (value)=>{
+    console.log("in")
+    console.log(value)
+    const profileref= doc(db,"Credential",sessionStorage.getItem("userid"))
+    await updateDoc(profileref,{
+      SavedClass:arrayRemove(value)
+    })
+rando()}
+
+    return (
+        <>
+            <NavbarLoggedIn />
+            <div class="dash-full-screen-container">
+                <div class="dashboard-container">
+                    <div class="card-grid">
+
+                        {saveCl.map((result) => (
+                            <div class="dash-class-cards" key={result.className}>
+                                <div class="dash-class-card-header card-image">
+                                    <img src={result.pic} />{result.className}
+                                </div>
+                                <div class="dash-class-card-body">{result.title}</div>
+                                <div class="gotoButtonDiv">
+                                    <button className="class-page-button" onClick={() => goToClass(result.title, result.className)}>go to class!</button>
+                                    <button className="class-page-button" onClick={() => removeSavedPost(result)}>remove this class!</button>
+                                </div>
+                            </div>
+                        ))}
+
+                    </div>
+                    <div class="side-bar-form" >
+                        <h1 class="dashboard-title">Dashboard</h1>
+
+                        <div class="dash-profile-card">
+                            <form>
+                                <h1 class="dash-name">{sessionStorage.getItem("username")}</h1>
+                            </form>
+                        </div>
+
+                        <div class="dash-button">
+                            <button type="submit" onClick={navigateToProfile}>My Profile</button>
+                        </div>
+
+                        <div class="dash-button">
+                            <button type="submit" onClick={navigateToNotes}>My Notes</button>
+                        </div>
+
+
+
+                        <div class="dash-button">
+                            <button type="submit" onClick={navigateToLogout}>Logout</button>
+                        </div>
+
+                    </div>
+
+
+
+                </div>
+            </div>
+        </>
+    )
+}
